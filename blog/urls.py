8ee84f48b2fabf8f6fe4b07e8a2cde4730bb1e9e
Seifeddine Selmi <selmi.seifeddine19@gmail.com>
2017-02-16 11:15:59 -0500

    Clean blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,20 +4,19 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^home', views.home),
-    url(r'^article/(?P<id>\d+)$', views.view, name="view_article"),
-    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_articles),
 
-    url(r'^redirection$', views.view_redirection),
-
-    url(r'^date$', views.date_actuelle),
-    url(r'^addition/(?P<nombre1>\d+)/(?P<nombre2>\d+)/$', views.addition),
-    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^article/(?P<id>\d+)$', views.view_article, name="view_article"),
     url(r'^add_article/$', views.add_article, name='add_article'),
     url(r'^edit_article/(?P<id>\d+)$', views.edit_article, name='edit_article'),
     url(r'^delete_article/(?P<id>\d+)$', views.delete_article, name='delete_article'),
+
     url(r'^new_contact/$', views.new_contact, name='new_contact'),
     url(r'^contact_list/$', views.contact_list, name='contact_list'),
+
     url(r'^new_document/$', views.new_document, name='new_document'),
     url(r'^document_list/$', views.document_list, name='document_list'),
+
+    url(r'^contact/$', views.contact, name='contact'),
+
+    url('faq', views.faq, name='faq'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,81 +13,22 @@ def index(request):
     return render(request, 'blog/index.html', {'articles': articles})
 
 
-def home(request):
-    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
-    text = """<h1>Bienvenue sur mon blog !</h1>"""
-    return HttpResponse(text)
-
-def view_article(request, id_article):
-    """
-    Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
-    Son ID est le second paramètre de la fonction (pour rappel, le premier
-    paramètre est TOUJOURS la requête de l'utilisateur)
-    """
-
-    if int(id_article) > 100:
-        #raise Http404
-        return redirect(view_redirection)
-    return HttpResponse(
-        "Vous avez demandé l'article #{0} !".format(id_article)
-    )
-
-def view_redirection(request):
-    return HttpResponse("Article n'existe pas !!!.")
-
-def view(request, id):
+def view_article(request, id):
     article = get_object_or_404(Article, id=id)
-    return render(request, 'blog/view.html', {'article': article})
-
-
-
-def list_articles(request, month, year):
-    """ Liste des articles d'un mois précis. """
-
-    #return redirect('view_article', id_article=42)
-    return HttpResponse(
-        "Vous avez demandé les articles de {0} {1}.".format(month, year)
-    )
-
-def date_actuelle(request):
-    return render(request, 'blog/date.html', {'date': datetime.now()})
-
-
-def addition(request, nombre1, nombre2):
-    total = int(nombre1) + int(nombre2)
-
-    # Retourne nombre1, nombre2 et la somme des deux au tpl
-    return render(request, 'blog/addition.html', locals())
-
-
-def contact(request):
-
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-
-        subject = form.cleaned_data['subject']
-        message = form.cleaned_data['message']
-        from_email = form.cleaned_data['from_email']
-        resend = form.cleaned_data['resend']
-
-        send = True
-        if subject and message and from_email:
-           try:
-                send_mail(subject, message, from_email, ['selmi.seifeddine19@gmail.com'])
-           except BadHeaderError:
-               return HttpResponse('Invalid header found.')
-           return HttpResponseRedirect('/blog/contact/')
-
-    return render(request, 'blog/contact.html', locals())
-
+    return render(request, 'blog/view_article.html', {'article': article})
 
 def add_article(request):
     form = ArticleForm(request.POST or None)
+    save = False
     if form.is_valid():
         form.save()
+        save = True
         return HttpResponseRedirect('/blog/')
 
-    return render(request, 'blog/add.html', {'form': form})
+    return render(request, 'blog/add_article.html',{
+        'form': form,
+        'save': save
+    })
 
 
 def edit_article(request, id):
@@ -101,9 +42,10 @@ def edit_article(request, id):
     )
     if form.is_valid():
         form.save()
+        save = True
         return HttpResponseRedirect('/blog/')
 
-    return render(request, 'blog/edit.html', locals())
+    return render(request, 'blog/edit_article.html', locals())
 
 
 def delete_article(request, id):
@@ -152,5 +94,30 @@ def new_document(request):
 def document_list(request):
     documents = Document.objects.all()
     return render(request, 'blog/document_list.html', {'documents': documents})
+
+
+def contact(request):
+
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        from_email = form.cleaned_data['from_email']
+        resend = form.cleaned_data['resend']
+
+        send = True
+        if subject and message and from_email:
+           try:
+                send_mail(subject, message, from_email, ['selmi.seifeddine19@gmail.com'])
+           except BadHeaderError:
+               return HttpResponse('Invalid header found.')
+           return HttpResponseRedirect('/blog/contact/')
+
+    return render(request, 'blog/contact.html', locals())
+
+
+def faq(request):
+    return render(request, 'blog/faq.html', {})
 
 
