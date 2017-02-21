6fb75d0040a1f07576fce2bfd97af6ee55806a6d
Seifeddine Selmi <selmi.seifeddine19@gmail.com>
2017-02-21 11:16:22 -0500

    Update slug unique

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ import os
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(unique=True)
     author = models.CharField(max_length=42)
     content = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de publication")
